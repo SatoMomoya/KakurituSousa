447a6227fb3a833494724eb9f7966c326c8a772b
さとうももや <goodragon2158@gmail.com>
2019-04-16 12:55:07 +0900

    最初のコミット

---
+++ 確率操作/gathMachine/Assets/Scenes/Weapon/EnemyAttackZone.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttackZone : MonoBehaviour
+{
+    private EnemyController enemyController;
+    private Vector3 playerPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyController = FindObjectOfType<EnemyController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.tag == "Player")
+        {
+            {
+                //攻撃範囲に入ったら
+
+                enemyController.AttackFlag = true;
+                playerPos = other.transform.position;
+            }  
+        }
+    }
+
+    public Vector3 PlayerPos
+    {
+        get { return playerPos; }
+        set { playerPos = value; }
+    }
+}
---
+++ 確率操作/gathMachine/Assets/Scenes/Weapon/EnemyVision.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyVision : MonoBehaviour
+{
+    private EnemyController enemyController;
+    private Vector3 playerPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyController = FindObjectOfType<EnemyController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if(other.transform.tag == "Player")
+        {
+            //視界に入っている間
+                enemyController.VisionFlag = true;
+                playerPos = other.transform.position;
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.transform.tag == "Player")
+        {
+            //視界から出たら
+          
+                enemyController.VisionFlag = false;
+
+              
+        }
+    }
+
+    public Vector3 PlayerPos
+    {
+        get { return playerPos; }
+        set { playerPos = value; }
+    }
+}
