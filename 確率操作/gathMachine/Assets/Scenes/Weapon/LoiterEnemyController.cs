3d013a0f57829783932e0f09e701b37ba4d94c07
TanakaAyumu12 <shibibi247@gmail.com>
2019-05-19 16:49:10 +0900

    杖のモーションと攻撃

--- 確率操作/gathMachine/Assets/Scenes/Weapon/LoiterEnemyController.cs
+++ 確率操作/gathMachine/Assets/Scenes/Weapon/LoiterEnemyController.cs
@@ -24,7 +24,7 @@ public class LoiterEnemyController : Momoya.Enemy
     //初期化
     public override void Initialize()
     {
-
+        
         playerObj = GameObject.Find("Player");
         player = playerObj.GetComponent<Momoya.Player>();
 
